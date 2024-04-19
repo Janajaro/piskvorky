27aa3ba4219533662e9d1de0f6eabfce64ff90d7
Jana Jarošová <Janamjaros@seznam.cz>
2024-04-19 10:31:44 +0200

    úkol 4, javascript update

--- hra.js
+++ hra.js
@@ -1,3 +1,5 @@
+import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4';
+
 let currentPlayer = 'circle';
 
 const selectPole = (event) => {
@@ -11,35 +13,39 @@ const selectPole = (event) => {
     currentPlayer = 'circle';
     document.querySelector('.kolecko-white').src = 'circle-white.svg';
   }
+
+  const gameField = Array.from(document.querySelectorAll('.pole')).map(
+    (htmlButton) => {
+      if (htmlButton.classList.contains('board__field--cross')) {
+        return `x`;
+      } else if (htmlButton.classList.contains('board__field--circle')) {
+        return `o`;
+      } else {
+        return `_`;
+      }
+    },
+  );
+
+  const winner = findWinner(gameField);
+  if (winner === 'o') {
+    setTimeout(() => {
+      alert(`Vyhrál hráč se symbolem ${winner}.`);
+      location.reload();
+    }, 1000);
+  } else if (winner === 'x') {
+    setTimeout(() => {
+      alert(`Vyhrál hráč se symbolem ${winner}.`);
+      location.reload();
+    }, 1000);
+  } else if (winner === 'tie') {
+    setTimeout(() => {
+      alert('Oba jste looseři! Zahrajte si znovu!');
+      location.reload();
+    }, 1000);
+  }
 };
 
-document
-  .querySelector('button:nth-child(1)')
-  .addEventListener('click', selectPole);
-document
-  .querySelector('button:nth-child(2)')
-  .addEventListener('click', selectPole);
-document
-  .querySelector('button:nth-child(3)')
-  .addEventListener('click', selectPole);
-document
-  .querySelector('button:nth-child(4)')
-  .addEventListener('click', selectPole);
-document
-  .querySelector('button:nth-child(5)')
-  .addEventListener('click', selectPole);
-document
-  .querySelector('button:nth-child(6)')
-  .addEventListener('click', selectPole);
-document
-  .querySelector('button:nth-child(7)')
-  .addEventListener('click', selectPole);
-document
-  .querySelector('button:nth-child(8)')
-  .addEventListener('click', selectPole);
-document
-  .querySelector('button:nth-child(9)')
-  .addEventListener('click', selectPole);
-document
-  .querySelector('button:nth-child(10)')
-  .addEventListener('click', selectPole);
+const allButtonsSelection = document.querySelectorAll('.pole');
+allButtonsSelection.forEach((button) => {
+  button.addEventListener('click', selectPole);
+});
